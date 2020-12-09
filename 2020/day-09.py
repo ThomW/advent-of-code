172ff136f1c37236d8cdc2c4b5f1e2455b602b9a
Thom Wetzel <thom.wetzel@gmail.com>
2020-12-09 12:38:55 -0500

    Day 9

---
+++ 2020/day-09.py
@@ -0,0 +1,65 @@
+test_data = [
+35
+,20
+,15
+,25
+,47
+,40
+,62
+,55
+,65
+,95
+,102
+,117
+,150
+,182
+,127
+,219
+,299
+,277
+,309
+,576
+]
+
+def test_pairs(data, tgt):
+  for i in range(0, len(data)):
+    for j in range(0, len(data)):
+      if i == j:
+        continue
+      elif data[i] + data[j] == tgt:
+        return True
+  return False
+
+def find_weak_link(data, preamble_size):
+  for idx in range(preamble_size, len(data)):
+    tgt = data[idx]
+    if not test_pairs(data[idx - preamble_size:idx], tgt):
+      return tgt
+
+def part_two(data, tgt):
+  for idx, val in enumerate(data):
+    idx_add = 1
+    while True:
+      current_set = data[idx:idx+idx_add]
+      sum_set = sum(current_set)
+      # print('{}: {}'.format(sum_set, current_set))      
+      if sum_set == tgt:
+        return min(data[idx:idx+idx_add]) + max(data[idx:idx+idx_add])
+      elif sum_set > tgt:
+        break
+      elif idx_add + idx > len(data):
+        break
+      else:
+        idx_add += 1
+
+  return None
+
+with open ('day-09.txt', 'r') as f:
+  numbers = [int(i) for i in f.readlines()]
+
+print ('Test Part One: {}'.format(find_weak_link(test_data, 5)))
+part_one_solution = find_weak_link(numbers, 25)
+print('Part One: {}'.format(part_one_solution))
+
+print('Test Part Two: {}'.format(part_two(test_data, 127)))
+print('Part Two: {}'.format(part_two(numbers, part_one_solution)))
