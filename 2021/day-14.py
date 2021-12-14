2f119b3dc743f1804cb040be81b56bf9f3dd3606
Thom Wetzel <thom.wetzel@gmail.com>
2021-12-14 13:24:38 -0500

    I had to look at some others' work to figure out 14, but I wrote then rewrote it from memory a few hours later to make sure I retained it

--- 2021/day-14.py
+++ 2021/day-14.py
@@ -1,3 +1,5 @@
+from typing import DefaultDict
+
 example = """NNCB
 
 CH -> B
@@ -64,8 +66,71 @@ def part_one(data, num_steps):
 
 	return highest - lowest
 
-print('Part 1 Example (Target 1588): {}'.format(part_one(example, 10)))
-print('Part 1 Solution: {}'.format(part_one(puzzle_input, 10)))
+# Brute force collapses when things get too complicated, so this is
+# a smarter approach to the problem
+def part_two(data, steps):
+	start, rules = process_input(data)
+
+	print()
+	print(start)
+
+	# Initialize letter counter
+	letters = DefaultDict(int)
+	for char in start:
+		letters[char] += 1
+
+	# Convert start string into a map where key is letter combo, value is frequency
+	pairs = DefaultDict(int)
+	for n in range(0, len(start) - 1):
+		pairs[start[n:n+2]] = 1
+
+	# Loop through steps incrementing counters for matching pairs
+	for step in range(0, steps):
+		new_pairs = DefaultDict(int)
+		for pair, frequency in pairs.items():
+
+			# Follow rules to inject new values in the middle of pairs
+			new_pairs[pair[0] + rules[pair]] += frequency
+			new_pairs[rules[pair] + pair[1]] += frequency
+
+			# Keep track of the new letters introduced into the overall string
+			letters[rules[pair]] += frequency
+
+		pairs = new_pairs
+
+	return max(letters.values()) - min(letters.values())
+
+# This is a rewrite of part_two that I wrote from memory using what
+# I learned. 
+def part_three(input, steps):
+
+	start, rules = process_input(input)
+
+	pairs = DefaultDict(int)
+	for n in range(0, len(start) - 1):
+		pairs[start[n:n+2]] = 1
+
+	letters = DefaultDict(int)
+	for char in start:
+		letters[char] += 1
+
+	for n in range(0, steps):
+		new_pairs = DefaultDict(int)
+		for pair, frequency in pairs.items():
+			rule = rules[pair]
+			new_pairs[pair[0] + rule] += frequency
+			new_pairs[rule + pair[1]] += frequency
+			letters[rule] += frequency
+		pairs = new_pairs
+
+	return max(letters.values()) - min(letters.values())
+
+	# This looks just like my previous part_two and the 
+	# values are the same, so I retained something ... haha
+
+print('Part 1 Example (Target 1588): {}'.format(part_three(example, 10)))
+print('Part 1 Solution: {}'.format(part_three(puzzle_input, 10)))
 
 # Definitely need to find a better solution. Maybe look back at day-06 (lanternfly)?
-print('Part 2 Example (Target 2188189693529): {}'.format(part_one(example, 40)))
+print('Part 2 Example (Target 2188189693529): {}'.format(part_three(example, 40)))
+print('Part 2 Solution: {}'.format(part_three(puzzle_input, 40)))
